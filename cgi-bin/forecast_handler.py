ad3e04f1c61fbc3d63683ad59a21ede4f1e261c4
Stanley, Simon <simon.stanley@metoffice.gov.uk>
2015-03-25 15:25:34 +0000

    changed link

--- cgi-bin/forecast_handler.py
+++ cgi-bin/forecast_handler.py
@@ -26,8 +26,7 @@ import numpy
 import os
 import json
 
-import_directory = '/home/h02/frgo/TEST/jhirst_plots/new_caboff_plots'\
-                   '/plots_N216/'
+import_directory = '../example_data/'
 export_directory = '/net/windows/m-drive/metoffice/Production/Operations_Centre/_Public_Write/3_Month_Outlook/Exported_Data/'
 temp_directory   = '/home/h02/sstanley/temp/'
 
